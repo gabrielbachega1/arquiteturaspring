dba8fefb20ebe37ebdb6bf687f8459e5bdf18d30
gabrielbachega1 <bachegagabriel@gmail.com>
2025-08-20 11:03:33 -0300

    beans primary e qualifiers

--- src/main/java/io/github/gabrielbachega1/arquiteturaspring/montadora/api/TesteFabricaController.java
+++ src/main/java/io/github/gabrielbachega1/arquiteturaspring/montadora/api/TesteFabricaController.java
@@ -2,14 +2,19 @@ package io.github.gabrielbachega1.arquiteturaspring.montadora.api;
 
 import io.github.gabrielbachega1.arquiteturaspring.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/carros")
 public class TesteFabricaController {
 
     @Autowired
+    //@Qualifier("motorTurbo")
+    @Aspirado
     private Motor motor;
 
     @PostMapping
